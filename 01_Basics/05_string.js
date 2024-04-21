0e43e9db72269e3fd5234cd1eaa161aee89323a8
Sumit Kumar Sinha <116585363+sumitkr739@users.noreply.github.com>
2024-04-21 07:24:52 +0000

    array,objects,strings

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,9 @@
+const name = "sumit";
+const repoCount = 50;
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const getName = new String('sumit');
+
+console.log(getName[0]);
+console.log(getName.toUpperCase());

8426de24da6f3b4fabd6537e52488ddca0f61748
Sumit Kumar Sinha <sumitsinha739@gmail.com>
2024-09-11 22:40:17 +0530

    js_revision

---
+++ Day_01.js/Math_objects.js
@@ -0,0 +1,11 @@
+//Math object built in object in javascript
+
+// console.log(Math);
+
+let number = 7.2;
+console.log(Math.round(number));
+console.log(Math.floor(number)); //round to down
+console.log(Math.ceil(number)); //round to up
+
+let random = Math.random();
+console.log(random*100);
---
+++ Day_01.js/for_each_loop.js
@@ -0,0 +1,12 @@
+let dishes = ["Biryani", "Samosa", "chips", "fries"];
+
+//using for loop
+for (let i = 0; i < dishes.length; i++) {
+  console.log(dishes[i]);
+}
+
+//USing foreach loop
+dishes.forEach(function (element) {
+  console.log(element);
+});
+
